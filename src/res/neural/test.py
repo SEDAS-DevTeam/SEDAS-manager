42367bb177aa58c6abd37d82965322ad20509932
HelloWorld7894 <danpojhan@protonmail.com>
2023-10-09 19:26:25 +0200

    whisper integration with microphone

--- src/res/neural/test.py
+++ src/res/neural/test.py
@@ -8,7 +8,6 @@ r_instance = redis.Redis(host='localhost', port=6379, decode_responses=True)
 while True:
     value = r_instance.get("start-voice")
     if value == "true": #start recognition
-        
         r_instance.set("out-voice", "test string")
     elif value == "false": #stop recognition
         r_instance.set("out-voice", "none")

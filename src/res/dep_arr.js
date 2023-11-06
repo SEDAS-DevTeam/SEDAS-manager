fc5ad28437b36a686dd2443cfb1b44c87e18e8ac
HelloWorld7894 <danpojhan@protonmail.com>
2023-11-06 11:32:03 +0100

    added time

--- src/res/dep_arr.js
+++ src/res/dep_arr.js
@@ -1,3 +1,16 @@
 window.onload = () => {
-    
+    setInterval(update_time, 1000)
+}
+
+function update_time(){
+    let time_elem = document.getElementById("top-time-header")
+    let date = new Date()
+    let date_utc = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(),
+    date.getUTCDate(), date.getUTCHours(),
+    date.getUTCMinutes(), date.getUTCSeconds());
+
+    let innerhtml = "Time: " + new Date(date_utc).toUTCString()
+
+    time_elem.innerHTML = innerhtml
+
 }

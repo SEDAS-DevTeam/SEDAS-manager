b4d200d8ff8fb6ae8c1db0363aada29bf8fff711
HelloWorld7894 <danpojhan@protonmail.com>
2023-11-14 20:37:19 +0100

    added database functions (NOT TESTED)

--- src/database.ts
+++ src/database.ts
@@ -2,18 +2,43 @@ import * as sqlite from "sqlite3"
 
 const DB = new sqlite.Database(":plane_info:")
 
-DB.serialize(() => {
-    DB.run("CREATE TABLE lorem (info TEXT)");
 
-    const stmt = DB.prepare("INSERT INTO lorem VALUES (?)");
-    for (let i = 0; i < 10; i++) {
-        stmt.run("Ipsum " + i);
-    }
-    stmt.finalize();
+export function CreateDatabase(){
+    DB.run("CREATE TABLE PLANES (id INTEGER, callsign TEXT, heading INTEGER, level INTEGER, speed INTEGER, departure TEXT, arrival TEXT)");
+}
 
-    DB.each("SELECT rowid AS id, info FROM lorem", (err, row: any) => {
-        console.log(row.id + ": " + row.info);
-    });
-});
+export function InsertRecord(id: number, callsign: string, heading: number, 
+                            level: number, speed: number, departure: string, arrival: string){
+    
+    DB.run(`INSERT INTO PLANES (${id}, ${callsign}, ${heading}, ${level}, ${speed}, ${departure}, ${arrival})`, 
+        (err) => {
+            if (err == null){
+                console.log(err)
+            }
+        }
+    )
+}
 
-DB.close();
+export function DeleteRecord(id: number){
+    DB.run(`DELETE FROM PLANES WHERE id='${id}'`, (err) => {
+        if (err == null){
+            console.log(err)
+        }
+    })
+}
+
+export function SelectRecord(id: number){
+    DB.run(`SELECT * FROM PLANES WHERE id=${id}`, (err, row: any) => {
+        if (err == null){
+            console.log(err)
+        }
+        else{
+            console.log(row)
+        }
+    })
+}
+
+export function CloseDatabase(){
+    DB.run("DROP DATABASE PLANES")
+    DB.close();
+}

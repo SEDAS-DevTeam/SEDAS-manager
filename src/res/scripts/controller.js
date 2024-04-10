f1ddd34117b605f191cc308be04b74fffb325228
HelloWorld7894 <danpojhan@protonmail.com>
2024-04-10 22:36:59 +0200

    added wiki, tomorrow I will work on drop downs and finishing core.py

--- src/res/scripts/controller.js
+++ src/res/scripts/controller.js
@@ -31,6 +31,12 @@ var monitor_data = [] //data for storing monitors
 var plane_data = [] //data for storing all planes
 var map_checked = false
 
+//wiki variables
+var sources = [
+    "https://sedas-docs.readthedocs.io/en/latest/",
+    "https://wiki.ivao.aero/en/home"
+]
+
 /*
 CHOOSING WHICH MAP TO GENERATE ON WHICH MONITOR
 */
@@ -926,6 +932,36 @@ window.onload = () => {
                     ChangeTime(-1, i)
                 })
             }
+            break
+        case "wiki.html":
+            let iframe_buttons = document.getElementsByClassName("change-iframe")
+
+            //always try to load sedas docs
+            if (window.navigator.onLine){
+                document.getElementById("wiki-content").src = sources[0]
+                iframe_buttons[0].classList.add("selected")
+            }
+            else{
+                document.getElementById("wiki-content").src = "./internet.html"
+                break //do not allow to set listeners for iframe buttons
+            }
+
+            for (let i = 0; i < iframe_buttons.length; i++){
+                iframe_buttons[i].addEventListener("click", (event) => {
+                    //remove all residual class lists
+                    for (let elem of iframe_buttons) {
+                        console.log(elem)
+                        if (elem.classList.contains("selected")){
+                            elem.classList.remove("selected")
+                        }
+                    }
+
+                    event.target.classList.add("selected")
+
+                    document.getElementById("wiki-content").src = sources[i]
+                })
+            }
+            break
     }
 
     //set for all pages
